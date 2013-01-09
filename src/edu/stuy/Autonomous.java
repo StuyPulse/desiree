34d7fab624b13ae663f59534a3ad47514fc61a41
Eric Lam <captainpicardlam@gmail.com>
2013-01-09 17:02:06 -0500

    Add Autonomous Class.

---
+++ src/edu/stuy/Autonomous.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stuy;
+
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * This is the location of the autonomous routines.
+ * @author Eric
+ */
+public class Autonomous {
+    
+    Dripto drip;
+    
+    public Autonomous(Dripto d) {
+        drip = d;
+    }
+    
+}
