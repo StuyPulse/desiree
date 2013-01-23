8fc0ca2d6e10febdfd7d7a932a5d2558a2f1815c
Justin Yulli Kim <yulli@yulli.org>
2013-01-22 20:15:22 -0500

    Add two definite auton cases and pseudo-code for each

--- src/edu/stuy/Autonomous.java
+++ src/edu/stuy/Autonomous.java
@@ -6,6 +6,8 @@ package edu.stuy;
 
 /**
  * This is the location of the autonomous routines.
+ * We assume that we always begin touching the front edge of the pyramid.
+ * This gives us two discs.
  * @author Eric
  */
 public class Autonomous {
@@ -16,10 +18,41 @@ public class Autonomous {
     
     public static void run() {
         switch (getAutonSetting()) {
-            case 1:
+            case 1: // just shoot two discs
+                auton1();
+                break;
+            case 2: // shoot two discs, back up to pick up 4, move forward, and shoot again
+                auton2();
                 break;
             default:
                 break;
         }
+    }
+    
+    
+    /**
+     * Shoot two discs and then do nothing
+     */
+    public static void auton1() {
+        // get shooter ready to shoot
+        // shoot first disc
+        // wait for shooter to be ready again
+        // shoot second disc
+    }
+    
+    /**
+     * Shoot two discs, then move backwards under the pyramid to pick up the
+     * four in the middle of the field, move back to the front edge of the
+     * pyramid and shoot the four discs.
+     */
+    public static void auton2() {
+        // get shooter ready to shoot
+        // shoot first disc
+        // wait for shooter to be ready again
+        // shoot second disc
+        // back up a certain distance and acquire at a certain speed to pick up
+            // the four discs
+        // move back up the same distance
+        // wait for shooter and shoot and repeat
     }
 }
