99aae8ae8a769fa8b5a9c73300e79989f581f1d3
arfan308 <areha308@gmail.com>
2013-01-23 20:29:16 -0600

    Create dummy code for non-existent linear/skeet shooter

--- src/edu/stuy/subsystems/Shooter.java
+++ src/edu/stuy/subsystems/Shooter.java
@@ -13,16 +13,23 @@ import edu.wpi.first.wpilibj.Talon;
  * @author Arfan
  */
 public class Shooter {
-    //dummy code
+// dummy code
     private static Shooter instance;
     private Talon shooter;
     private Talon tilter;
     private AnalogChannel pot;
     
-    private Shooter () {
+    private Shooter() {
         shooter = new Talon(Constants.SHOOTER_CHANNEL);
         tilter = new Talon(Constants.TILTER_CHANNEL);
         pot = new AnalogChannel(Constants.POT_CHANNEL);
+    }
+    
+    public static Shooter getInstance() {
+        if (instance == null) {
+            instance = new Shooter();
+        }
+        return instance;
     }
     
     public void tilt(double val) {
