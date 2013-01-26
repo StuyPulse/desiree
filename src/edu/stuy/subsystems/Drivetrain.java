6a19343b026c634cd39619556572e3e462a54301
Prog 694 <prog694@gmail.com>
2013-01-26 13:21:17 -0500

    Finished theoretical drive straight code ready for testing.

--- src/edu/stuy/subsystems/Drivetrain.java
+++ src/edu/stuy/subsystems/Drivetrain.java
@@ -6,19 +6,35 @@ package edu.stuy.subsystems;
 
 import edu.stuy.Constants;
 import edu.stuy.util.Gamepad;
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 /**
  * The robot drivetrain.
  * @author kevin
  */
-public class Drivetrain {
+public class Drivetrain{
     private static Drivetrain instance;
     
-    private RobotDrive drivetrain;
+    PIDController straightController;
+    Gyro gyro;
     
+    private RobotDrive drivetrain;
+
     private Drivetrain() {
         drivetrain = new RobotDrive(Constants.DRIVETRAIN_LEFT_CHANNEL, Constants.DRIVETRAIN_RIGHT_CHANNEL);
+        gyro = new Gyro(Constants.GYRO_CHANNEL);
+                
+        straightController = new PIDController(Constants.PVAL, Constants.IVAL, Constants.DVAL, gyro, new PIDOutput() {
+            public void pidWrite(double output) {
+               drivetrain.arcadeDrive(.1, output);
+            }
+        }, 0.005);
+        straightController.setInputRange(-360.0, 360.0);
+        straightController.setPercentTolerance(1 / 90. * 100);
+        straightController.disable();
     }
     
     public static Drivetrain getInstance() {
@@ -38,5 +54,14 @@ public class Drivetrain {
      */
     public void tankDrive(Gamepad gamepad) {
         tankDrive(-gamepad.getLeftY(), -gamepad.getRightY());
+    }
+
+    public void enableDriveStraight(){
+           straightController.setSetpoint(0);
+           straightController.enable();
+    }
+
+    public void disableDriveStraight(){
+       straightController.disable();
     }
 }
