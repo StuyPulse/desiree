d1546fa13d5dd5ca392730441401ac843d61b14a
Eric Lam <captainpicardlam@gmail.com>
2013-02-05 17:44:22 -0500

    Add relay controllers for lights to simplify.

--- src/edu/stuy/subsystems/Lights.java
+++ src/edu/stuy/subsystems/Lights.java
@@ -43,6 +43,28 @@ public class Lights {
     public Relay signalColorLight() {
         return colorSignal;
     }
+    
+    public void enableCameraLight(boolean on) {
+        if(on)
+            cameraLight.set(Relay.Value.kOn);
+        else
+            cameraLight.set(Relay.Value.kOff);
+    }
+    
+    public void enableWhiteLight(boolean on) {
+        if(on)
+            whiteSignal.set(Relay.Value.kOn);
+        else
+            whiteSignal.set(Relay.Value.kOff);
+    }
+    
+    public void enableColorLight(boolean on) {
+        if(on)
+            colorSignal.set(Relay.Value.kOn);
+        else
+            colorSignal.set(Relay.Value.kOff);
+    }
+    
     /*
      * run this in both tele and auton
      * flash when disc is picked up from ground or feeder
