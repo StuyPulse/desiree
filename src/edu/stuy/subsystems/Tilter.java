6c772122ce443e4f087878e39d8a997b119c6281
arfan308 <areha308@gmail.com>
2013-02-05 18:05:00 -0500

    Fix accel code

--- src/edu/stuy/subsystems/Tilter.java
+++ src/edu/stuy/subsystems/Tilter.java
@@ -18,16 +18,13 @@ import java.util.Vector;
  * @author kevin
  */
 public class Tilter {
+    
     private static Tilter instance;
     private Talon tilter;
     private ADXL345_I2C accel;
-    
     private Vector measurements10;
-    private Vector measurements100;
     private Timer updateMeasurements10;
-    private Timer updateMeasurements100;
     private final int UPDATE_PERIOD_MS10 = 10;
-    private final int UPDATE_PERIOD_MS100 = 100;
     
     private Tilter() {
         tilter = new Talon(Constants.TILTER_CHANNEL);
@@ -71,32 +68,18 @@ public class Tilter {
                 }
             }
         }, 0, UPDATE_PERIOD_MS10);
-        updateMeasurements100 = new Timer();
-        updateMeasurements100.schedule(new TimerTask() {
-
-            public void run() {
-                synchronized (measurements100) {
-                    measurements100.addElement(new Double(getAbsoluteAngle()));
-                    if (measurements100.size() > 100) {
-                        measurements100.removeElementAt(0);
-                    }
-                }
-            }
-        }, 0, UPDATE_PERIOD_MS100);
+        
     }
     
     public void accelStop() {
-        if (updateMeasurements10 != null) {
+        if (updateMeasurements10 != null) 
             updateMeasurements10.cancel();
-        }
-        if (updateMeasurements100 != null) {
-            updateMeasurements100.cancel();
-        }
+        
     }
     
     public void reset() {
         measurements10.removeAllElements();
-        measurements100.removeAllElements();
+      
     }
     
     public double getXAcceleration() {
@@ -122,20 +105,6 @@ public class Tilter {
                 sum += ((Double) measurements10.elementAt(i)).doubleValue();
             }
             return sum / measurements10.size();
-        }
-    }
-    
-    /* Gets the angle from the measurements of the last 100 acceleration */
-    public double getAbsoluteAngle100() {
-        if (measurements100.isEmpty()) {
-            return 0;
-        }
-        double sum = 0;
-        synchronized (measurements100) {
-            for (int i = 0; i < measurements100.size(); i++) {
-                sum += ((Double) measurements100.elementAt(i)).doubleValue();
-            }
-            return sum / measurements100.size();
         }
     }
     
