e834a8928b774437c41562e9782ee6d474464f25
Justin Yulli Kim <yulli@yulli.org>
2013-02-15 18:02:35 -0500

    Fix up whitespace and styling NetworkIO

--- src/edu/stuy/util/NetworkIO.java
+++ src/edu/stuy/util/NetworkIO.java
@@ -3,7 +3,8 @@ package edu.stuy.util;
 import java.io.*;
 import javax.microedition.io.*;
 
-public class NetworkIO{
+public class NetworkIO {
+    
     private static final int PORT = 6940;
     private static final String HOST = "10.6.95.242";
     private SocketConnection requestSocket;
@@ -11,7 +12,8 @@ public class NetworkIO{
     private DataInputStream in;
     private String message;
     private double mostRecentOut;
-    public NetworkIO(){
+    
+    public NetworkIO() {
         mostRecentOut = 694; // return a huge value by default, the caller better know this
         try {
             SocketConnection requestSocket = (SocketConnection) Connector.open("socket://" + HOST + ":" + PORT);
@@ -23,8 +25,8 @@ public class NetworkIO{
             e.printStackTrace();
         }
     }
-    public void run()
-    {
+    
+    public void run() {
         try {
             double output;
             if (in.available() > 0) {
@@ -39,21 +41,21 @@ public class NetworkIO{
             e.printStackTrace();
         }
     }
-    void sendMessage(String msg)
-    {
-        try{
+    
+    void sendMessage(String msg) {
+        try {
             out.write(msg.getBytes());
             out.flush();
-        }
-        catch(IOException ioException){
+        } catch(IOException ioException) {
             ioException.printStackTrace();
         }
     }
-    public double getMostRecent () {
+    
+    public double getMostRecent() {
         return mostRecentOut;
     }
-    public double getCurrent()
-    {
+    
+    public double getCurrent() {
         run();
         return getMostRecent();
     }
