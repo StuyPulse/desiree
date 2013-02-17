ff00b26fc0127db155d45714a3f35a3b74e62a7a
Eric Lam <captainpicardlam@gmail.com>
2013-02-17 17:20:56 -0500

    Make climber 10-point. Comment out old stuff for possible future use.

--- src/edu/stuy/subsystems/Climber.java
+++ src/edu/stuy/subsystems/Climber.java
@@ -8,6 +8,7 @@ import edu.stuy.Constants;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.Solenoid;
 
 /**
  *
@@ -16,9 +17,11 @@ import edu.wpi.first.wpilibj.Victor;
 public class Climber {
     
     private static Climber instance;
-    private Victor wench;
-    private Servo deployer1;
-    private Servo deployer2;
+    //private Victor wench;
+    //private Servo deployer1;
+    //private Servo deployer2;
+    private Solenoid deployOut;
+    private Solenoid deployIn;
     
     
     public static Climber getInstance() {
@@ -29,35 +32,41 @@ public class Climber {
     }
     
     private Climber() {
-        wench = new Victor(Constants.WENCH_CHANNEL);
-        deployer1 = new Servo(Constants.CLIMBER_SERVO_1_CHANNEL);
-        deployer2 = new Servo(Constants.CLIMBER_SERVO_2_CHANNEL);
+        //wench = new Victor(Constants.WENCH_CHANNEL);
+        //deployer1 = new Servo(Constants.CLIMBER_SERVO_1_CHANNEL);
+        //deployer2 = new Servo(Constants.CLIMBER_SERVO_2_CHANNEL);
+        deployOut = new Solenoid(Constants.CLIMBER_DEPLOYER_CHANNEL);
+        deployIn = new Solenoid(Constants.CLIMBER_WITHDRAWER_CHANNEL);
     }
     
     public void forwardWench() {
-        wench.set(1);
+        //wench.set(1);
     }
     
     public void stopWench() {
-        wench.set(0);
+        //wench.set(0);
     }
     
     public void reverseWench() {
-        wench.set(-1);
+        //wench.set(-1);
     }
     
     public void setWench(double val) {
-        wench.set(val);
+        //wench.set(val);
     }
     
     public void deploy() {
-        deployer1.set(1.0);
-        deployer2.set(1.0);
+        //deployer1.set(1.0);
+        //deployer2.set(1.0);
+        deployIn.set(false);
+        deployOut.set(true);
     }
     
     public void undeploy() {
-        deployer1.set(0.0);
-        deployer2.set(0.0);
+        //deployer1.set(0.0);
+        //deployer2.set(0.0);
+        deployIn.set(true);
+        deployOut.set(false);
     }
     
     public void manualClimberControl(Joystick stick) {
